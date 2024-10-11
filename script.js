640eb15dea37e0abcaa5fe2160f3c15eb0023717
LucasTiriva <lucastiriva@gmail.com>
2024-10-11 18:15:26 -0300

    ajustando erros

--- script.js
+++ script.js
@@ -143,16 +143,18 @@ checkoutBtn.addEventListener("click", function() {
 function checkRestaurantOpen() {
  const data = new Date();
  const hora = data.getHours();
- return hora >= 18 && hora < 22
+ if(hora >= 18 && hora < 22) {
+    return true;
+ } else {
+    return false;
+ }
 }
 
-const spanItem = document.getElementById("date-sapn")
+const spanItem = document.getElementById("date-span")
 const isOpen = checkRestaurantOpen();
 
 if(isOpen){
-    spanItem.classList.remove("bg-red-500")
-    spanItem.classList.add("bg-green-600")
+    spanItem.classList.add("bg-green-600");
 } else {
-    spanItem.classList.remove("bg-green-600")
-    spanItem.classList.add("bg-red-500")
+    spanItem.classList.add("bg-red-500");
 }
